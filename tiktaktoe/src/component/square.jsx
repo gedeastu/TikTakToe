8d86393f09b28fb1b156c854d4d87ac5d8628f7b
gedeastu <gede.astu@gmail.com>
2023-07-29 19:05:57 +0800

    up

--- tiktaktoe/src/component/square.jsx
+++ tiktaktoe/src/component/square.jsx
@@ -1,14 +1,14 @@
 import React from 'react'
 import { useState } from 'react'
-const Square = () => {
-  const [X, setX] = useState(false)
-  const handleClickX = ()=>{
-    setX(!X ? ('X') : (null))
-  }
+const Square = ({value, onSquareClicked}) => {
+  // const [X, setX] = useState(false)
+  // const handleClickX = ()=>{
+  //   setX(!X ? ('X') : (null))
+  // }
   return (
     <>
-    <button onClick={handleClickX} className='w-10 h-10 border border-black text-[1.2rem] font-semibold cursor-pointer'>
-    {X}
+    <button onClick={onSquareClicked} className='w-10 h-10 border border-black text-[1.2rem] font-semibold cursor-pointer'>
+    {value}
     </button>
     </>
   )
